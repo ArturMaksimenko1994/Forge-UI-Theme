bcd0d3d60b164b902b15d697a66444f70c335801
Artur <arturmaksimenko784@gmail.com>
2026-02-26 23:49:34 +0300

    add theme-preset

--- functions.php
+++ functions.php
@@ -23,3 +23,40 @@ add_action( 'customize_register', function( $wp_customize ) {
     $wp_customize->remove_panel( 'nav_menus' );
 
 });
+
+
+function mytheme_add_body_class_theme($classes) {
+    // Получаем выбранную тему из настроек WP Customizer (по умолчанию default)
+    $theme_preset = get_theme_mod('theme_preset', 'default');
+
+    // Добавляем класс к body
+    $classes[] = 'theme-' . $theme_preset;
+
+    return $classes;
+}
+add_filter('body_class', 'mytheme_add_body_class_theme');
+
+function mytheme_customize_register($wp_customize) {
+    $wp_customize->add_section('theme_preset_section', array(
+        'title' => __('Выбор цветовой темы', 'mytheme'),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting('theme_preset', array(
+        'default' => 'default',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('theme_preset_control', array(
+        'label' => __('Цветовой пресет', 'mytheme'),
+        'section' => 'theme_preset_section',
+        'settings' => 'theme_preset',
+        'type' => 'radio',
+        'choices' => array(
+            'default' => __('Default', 'mytheme'),
+            'red' => __('Red', 'mytheme'),
+            'green' => __('Green', 'mytheme'),
+        ),
+    ));
+}
+add_action('customize_register', 'mytheme_customize_register');
